88a1db65a0d8cd94cb122cbde2036c763ccc4423
Maestro <cjm77v2b2v@privaterelay.appleid.com>
2026-08-14 17:26:37 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // jadi kasir tetap bisa jualan walau internet mati, lalu sinkron lagi
 // begitu online.
 
-const CACHE_NAME = 'maestro-pay-shell-v6';
+const CACHE_NAME = 'maestro-pay-shell-v7';
 
 // File dari domain sendiri
 const SHELL_FILES = [
